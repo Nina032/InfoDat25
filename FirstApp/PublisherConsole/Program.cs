2188c1cfdedd0fc7c0c0dbfe07f2688daaee6ebd
Nevena Kicanovic <nevena@contactus.se>
2025-12-29 14:05:30 +0100

    AddAuthors()

--- FirstApp/PublisherConsole/Program.cs
+++ FirstApp/PublisherConsole/Program.cs
@@ -4,3 +4,16 @@ using(PubContext context = new PubContext())
 {
     context.Database.EnsureCreated();
 }
+
+GetAuthors();
+
+
+void GetAuthors()
+{
+    using var context = new PubContext();
+    var authors = context.Authors.ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+}
